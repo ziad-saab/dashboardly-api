cbbca1c26cbc35586e593e96962d3ccebdf640af
Ziad Saab <ziad.saab@gmail.com>
2017-05-06 21:06:21 -0400

    added auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,16 +5,35 @@ const onlyLoggedIn = require('../lib/only-logged-in');
 module.exports = (dataLoader) => {
   const authController = express.Router();
 
+  // Create a new user (signup)
   authController.post('/users', (req, res) => {
-
+    dataLoader.createUser({
+      email: req.body.email,
+      password: req.body.password
+    })
+    .then(user => res.status(201).json(user))
+    .catch(err => res.status(400).json(err));
   });
 
+  // Create a new session (login)
   authController.post('/sessions', (req, res) => {
-
+    dataLoader.createTokenFromCredentials(
+      req.body.email,
+      req.body.password
+    )
+    .then(token => res.status(201).json({ token: token }))
+    .catch(err => res.status(401).json(err));
   });
 
+  // Delete a session (logout)
   authController.delete('/sessions', onlyLoggedIn(dataLoader), (req, res) => {
-
+    if (req.sessionToken === req.body.token) {
+      dataLoader.deleteToken(req.body.token)
+      .then(() => res.status(204).end())
+      .catch(err => res.status(400).json(err));
+    } else {
+      res.status(401).json({ error: 'Invalid session token' });
+    }
   });
 
   return authController;
