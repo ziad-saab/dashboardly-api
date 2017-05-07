c77fdf59417480b13164525b9759841dbec033bb
Ziad Saab <ziad.saab@gmail.com>
2017-05-06 21:16:09 -0400

    added bookmarks controller

--- controllers/bookmarks.js
+++ controllers/bookmarks.js
@@ -5,12 +5,12 @@ const onlyLoggedIn = require('../lib/only-logged-in');
 module.exports = (dataLoader) => {
   const bookmarksController = express.Router();
 
-  bookmarksController.patch('/:id', onlyLoggedIn(dataLoader), (req, res) => {
-
+  bookmarksController.patch('/:id', onlyLoggedIn, (req, res) => {
+    // TODO: this is up to you to implement :)
   });
 
-  bookmarksController.delete('/:id', onlyLoggedIn(dataLoader), (req, res) => {
-
+  bookmarksController.delete('/:id', onlyLoggedIn, (req, res) => {
+    // TODO: this is up to you to implement :)
   });
 
   return bookmarksController;
