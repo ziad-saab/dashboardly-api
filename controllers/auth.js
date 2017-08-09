7e1b7a9eb811e3ffbd49b9684bf204dfefd929db
SSaquif <ssaquif@gmail.com>
2017-08-09 13:32:38 -0400

    user info from session being returned

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,20 +19,14 @@ module.exports = (dataLoader) => {
 
   // Create a new session (login)
   authController.post('/sessions', (req, res) => {
-    //taking the input email and password as parameters
-    //createTokenFromCredentials returns the new sessionToken on success
+    //No need to use cookies or headers
+    //a token will be sent a json object
+    //AND will stored on a localStorage object provided by react/browser/front-end
     dataLoader.createTokenFromCredentials(req.body.email, req.body.password)
     .then(token => {
-      //TODO: Ask prof if doing this manually is the right way to do it
-      req.headers.authorization = "token " + token;
       return token;
     })
     .then(token => res.status(201).json({ token: token }))
-    //Above Sends a JSON response composed of a stringified version of the specified data
-    //And the req.headers gets something like token sdfkjsdnvskdjf
-    //This can used like below. See check-login-token.js
-    //const token = req.headers.authorization.split(' ')[1];
-    .then(()=>{console.log("req.headers from login post= ", req.headers)})
     .catch(err => res.status(401).json(err));
   });
 
@@ -52,7 +46,14 @@ module.exports = (dataLoader) => {
   // Retrieve current user
   authController.get('/me', onlyLoggedIn, (req, res) => {
     // TODO: this is up to you to implement :)
-    res.status(500).json({ error: 'not implemented' });
+    console.log("Headers ",req.headers);
+    console.log("Authorization = ",req.headers.authorization.split(' ')[1]);
+    dataLoader.getUserFromSession(req.headers.authorization.split(' ')[1])
+      .then(user => {
+        console.log("user =", user);
+        return user;
+      })
+      .then(user => res.status(201).json(user));
   });
 
   return authController;
