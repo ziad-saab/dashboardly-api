221c6bef879e8fad5927346b5937d9d20430ac6c
nebulaUnicornis <antonio.qfel@gmail.com>
2017-08-10 17:46:46 -0400

    Delete bookmark function implemented

--- index.js
+++ index.js
@@ -33,7 +33,6 @@ const app = express();
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE,PATCH");
-  //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header("Access-Control-Allow-Headers", "Content-Type, authorization");
   next();
 });
